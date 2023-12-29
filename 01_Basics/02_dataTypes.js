e0073fb7f386f7d1828052289a903b86508e1dbf
Subha85829 <91304887+Subha85829@users.noreply.github.com>
2023-12-29 09:25:10 +0000

    DataTypes done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") --> We are using Node.Js not browser
+
+console.log(3+3); console.log(); // Not a good practice as it not good for readibality
+
+
+let name = "Subhasish"; // String
+let age = 25; // int
+
+
+/* types of dataType 
+    1. numbers => 2 to power 53
+    2. bigint 
+    3. string => " "
+    4. null => standalone value / empty
+    5. boolean => true / false
+    6. undefined => 
+    7. symbol => unique
+    
+    non-premitive 
+    1. object;
+
+
+*/
+
+console.log(typeof age);
+console.log(typeof null); // type of Object 
+console.log(typeof undefined); // type of undefined
