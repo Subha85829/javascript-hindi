c23b8a2d8341b74f5125a9a255b5fe275a815cf3
Subhasish Modak <91304887+Subha85829@users.noreply.github.com>
2024-01-28 15:23:21 +0000

    few more ..

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,40 @@
+// Array 
+
+
+const myArr = [0,2,4,5,7,6];
+
+
+//Array Method 
+// push && pop
+// myArr.push(6);
+// console.log(`Popped element is : ${myArr.pop()}`);
+// console.log(myArr);
+
+// shift and unshift 
+
+// myArr.unshift(12);
+// console.log(myArr);
+
+// myArr.shift();
+// console.log(myArr);
+
+
+// few more
+
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(2));
+
+
+// Array - join! 
+
+// const newArray = myArr.join();
+// console.log(newArray);
+
+
+//Slice || splice
+
+const arr = [0,1,2,3,4,5];
+
+console.log("Original Array : " + arr);
+
+// s
